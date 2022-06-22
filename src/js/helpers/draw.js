018024470d5665440a88bb3a4241b0bb181a7b80
Rifat <reaget.acc@gmail.com>
2022-06-22 14:58:10 +0300

    MRR 2022.06.22 | Rewrited main app

--- src/js/helpers/draw.js
+++ src/js/helpers/draw.js
@@ -20,6 +20,21 @@ Draw.prototype.text = function(text, x, y, color, font) {
 }
 /**
  * 
+ * @param {String} text to display
+ * @param {Number} x position by x
+ * @param {Number} y position by y
+ * @param {String} textColor RGBA or HEX color in string `rgba(0, 0, 0, 1)` 
+ * @param {String} bgColor RGBA or HEX color in string `rgba(0, 0, 0, 1)` 
+ * @param {String} size font-weight: 600, font-size: size, font-family: monospace
+ */
+Draw.prototype.textOverlay = function(text, x, y, textColor, bgColor, size) {
+  let {width} = this.measureText(text);
+
+  this.rect(x, y, width + 20, size * 2, bgColor);
+  this.text(text, x + 10, y + size * 1.25, textColor, `600 ${size}px monospace`);
+}
+/**
+ * 
  * @param {Number} x 
  * @param {Number} y 
  * @param {Number} width 
@@ -29,6 +44,14 @@ Draw.prototype.text = function(text, x, y, color, font) {
 Draw.prototype.rect = function(x, y, width, height, color) {
   this.ctx.fillStyle = color;
   this.ctx.fillRect(x, y, width, height);
+}
+/**
+ * 
+ * @param {String} text 
+ * @returns Object with parameters
+ */
+Draw.prototype.measureText = function(text) {
+  return this.ctx.measureText(text);
 }
 
 export {Draw};
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
 import '../scss/main.scss';
-import { init } from './app';
+import App from './app';
 
-init();
+const app = new App();
